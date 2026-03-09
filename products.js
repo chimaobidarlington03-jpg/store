6d8083ec41acacdf4b89f1b819e0b2f31dbeae7b
chimaobidarlington03-jpg <chimaobidarlington03@gmail.com>
2026-03-09 11:23:55 +0100

    Create products.js
    Third

---
+++ products.js
@@ -0,0 +1,17 @@
+const products = [
+    {
+        name: "Sneakers",
+        price: 15000,
+        img: "https://via.placeholder.com/200"
+    },
+    {
+        name: "T-Shirt",
+        price: 5000,
+        img: "https://via.placeholder.com/200"
+    },
+    {
+        name: "Cap",
+        price: 3000,
+        img: "https://via.placeholder.com/200"
+    }
+];
