6e8e56d807679c0b8508b30e7b7d63a28de22292
chimaobidarlington03-jpg <chimaobidarlington03@gmail.com>
2026-03-09 11:31:04 +0100

    Create store.js
    Fourth

---
+++ store.js
@@ -0,0 +1,41 @@
+let cart = [];
+const phone = "2347049884342"; // Put your WhatsApp number
+
+const productList = document.getElementById("productList");
+
+products.forEach(product => {
+    const card = document.createElement("div");
+    card.className = "card";
+    card.innerHTML = `
+        <img src="${product.img}" />
+        <h3>${product.name}</h3>
+        <p class="price">₦${product.price}</p>
+        <button onclick="addToCart('${product.name}',${product.price})">Add to Cart</button>
+    `;
+    productList.appendChild(card);
+});
+
+function addToCart(name, price){
+    cart.push({name, price});
+    alert(name + " added to cart");
+}
+
+function checkout(){
+    if(cart.length == 0){
+        alert("Your cart is empty");
+        return;
+    }
+
+    let message = "Hello, I want to order:\n";
+    let total = 0;
+
+    cart.forEach(item => {
+        message += `${item.name} - ₦${item.price}\n`;
+        total += item.price;
+    });
+
+    message += "Total: ₦" + total;
+
+    let url = `https://wa.me/${phone}?text=` + encodeURIComponent(message);
+    window.open(url, "_blank");
+}
