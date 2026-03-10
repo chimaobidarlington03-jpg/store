438d488e1fafe0dc5b6fd9b644dc6c60b6e04f11
chimaobidarlington03-jpg <chimaobidarlington03@gmail.com>
2026-03-10 08:57:59 +0100

    Update store.js

--- store.js
+++ store.js
@@ -1,44 +1,64 @@
-
 let cart = [];
-let savedProducts = JSON.parse(localStorage.getItem("products")) || [];
-products.push(...savedProducts);
-const phone = "2347049884342"; // Put your WhatsApp number
 
-const productList = document.getElementById("productList");
+const phone = "2347049884342"; // your WhatsApp number
 
-products.forEach(product => {
-    const card = document.createElement("div");
-    card.className = "card";
-    card.innerHTML = `
-        <img src="${product.img}" />
-        <h3>${product.name}</h3>
-        <p class="price">₦${product.price}</p>
-        <button onclick="addToCart('${product.name}',${product.price})">Add to Cart</button>
-    `;
-    productList.appendChild(card);
-});
 
-function addToCart(name, price){
-    cart.push({name, price});
-    alert(name + " added to cart");
+fetch("https://docs.google.com/spreadsheets/d/1h1AOC9Y7Kp-jTobt4DKLOnFtBY5gzo9Me87qJp2i7LI/edit?usp=drivesdk")
+.then(res => res.text())
+.then(data => {
+
+let rows = data.split("\n")
+
+rows.slice(1).forEach(row => {
+
+let cols = row.split(",")
+
+let name = cols[0]
+let price = cols[1]
+let img = cols[2]
+
+let card = document.createElement("div")
+
+card.className = "card"
+
+card.innerHTML = `
+<img src="${img}">
+<h3>${name}</h3>
+<p class="price">₦${price}</p>
+<button onclick="addToCart('${name}',${price})">Add to Cart</button>
+`
+
+document.getElementById("productList").appendChild(card)
+
+})
+
+})
+
+
+function addToCart(name,price){
+cart.push({name,price})
+alert(name + " added to cart")
 }
 
 function checkout(){
-    if(cart.length == 0){
-        alert("Your cart is empty");
-        return;
-    }
 
-    let message = "Hello, I want to order:\n";
-    let total = 0;
+if(cart.length==0){
+alert("Your cart is empty")
+return
+}
 
-    cart.forEach(item => {
-        message += `${item.name} - ₦${item.price}\n`;
-        total += item.price;
-    });
+let message="Hello, I want to order:\n"
+let total=0
 
-    message += "Total: ₦" + total;
+cart.forEach(item=>{
+message+=item.name+" - ₦"+item.price+"\n"
+total+=item.price
+})
 
-    let url = `https://wa.me/${phone}?text=` + encodeURIComponent(message);
-    window.open(url, "_blank");
+message+="Total: ₦"+total
+
+let url="https://wa.me/"+phone+"?text="+encodeURIComponent(message)
+
+window.open(url,"_blank")
+
 }
