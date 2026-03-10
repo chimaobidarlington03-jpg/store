660a83afb2587b0d320550a2d0a01a0417a17ad2
chimaobidarlington03-jpg <chimaobidarlington03@gmail.com>
2026-03-10 07:15:27 +0100

    Update products.js

--- products.js
+++ products.js
@@ -1,17 +1,3 @@
 const products = [
-    {
-        name: "Sneakers",
-        price: 15000,
-        img: "https://via.placeholder.com/200"
-    },
-    {
-        name: "T-Shirt",
-        price: 5000,
-        img: "https://via.placeholder.com/200"
-    },
-    {
-        name: "Cap",
-        price: 3000,
-        img: "https://via.placeholder.com/200"
-    }
+   
 ];
