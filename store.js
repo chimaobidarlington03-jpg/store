8ca66a426ff55f6a55481ab5191dea4e1abb11fb
chimaobidarlington03-jpg <chimaobidarlington03@gmail.com>
2026-03-10 10:30:18 +0100

    Update store.js

--- store.js
+++ store.js
@@ -1,50 +1,64 @@
 let cart = [];
-const phone = "2348123456789"; // your WhatsApp number
 
-// Replace this link with your CSV link
-fetch("https://docs.google.com/spreadsheets/d/1h1AOC9Y7Kp-jTobt4DKLOnFtBY5gzo9Me87qJp2i7LI/gviz/tq?tqx=out:csv")
+const phone = "2347049884342"; // your WhatsApp number
+
+
+fetch("PASTE-YOUR-GOOGLE-SHEET-LINK")
 .then(res => res.text())
 .then(data => {
 
-    let rows = data.split("\n");
-    rows.slice(1).forEach(row => {
-        let cols = row.split(",");
-        let name = cols[0];
-        let price = cols[1];
-        let img = cols[2];
+let rows = data.split("\n")
 
-        let card = document.createElement("div");
-        card.className = "card";
-        card.innerHTML = `
-            <img src="${img || 'https://via.placeholder.com/200'}" alt="${name}">
-            <h3>${name}</h3>
-            <p class="price">₦${price}</p>
-            <button onclick="addToCart('${name}', ${price})">Add to Cart</button>
-        `;
-        document.getElementById("productList").appendChild(card);
-    });
-});
+rows.slice(1).forEach(row => {
 
-function addToCart(name, price){
-    cart.push({name, price});
-    alert(name + " added to cart");
+let cols = row.split(",")
+
+let name = cols[0]
+let price = cols[1]
+let img = cols[2]
+
+let card = document.createElement("div")
+
+card.className = "card"
+
+card.innerHTML = `
+<img src="${img}">
+<h3>${name}</h3>
+<p class="price">₦${price}</p>
+<button onclick="addToCart('${name}',${price})">Add to Cart</button>
+`
+
+document.getElementById("productList").appendChild(card)
+
+})
+
+})
+
+
+function addToCart(name,price){
+cart.push({name,price})
+alert(name + " added to cart")
 }
 
 function checkout(){
-    if(cart.length==0){
-        alert("Your cart is empty");
-        return;
-    }
 
-    let message="Hello, I want to order:\n";
-    let total=0;
+if(cart.length==0){
+alert("Your cart is empty")
+return
+}
 
-    cart.forEach(item=>{
-        message+=item.name+" - ₦"+item.price+"\n";
-        total+=parseFloat(item.price);
-    });
+let message="Hello, I want to order:\n"
+let total=0
 
-    message+="Total: ₦"+total;
-    let url="https://wa.me/"+phone+"?text="+encodeURIComponent(message);
-    window.open(url,"_blank");
+cart.forEach(item=>{
+message+=item.name+" - ₦"+item.price+"\n"
+total+=item.price
+})
+
+message+="Total: ₦"+total
+
+let url="https://wa.me/"+phone+"?text="+encodeURIComponent(message)
+
+window.open(url,"_blank")
+
 }
